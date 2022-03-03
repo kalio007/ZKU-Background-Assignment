9affd212cb6b91dbdbb1d77b5d65c89aaae2a8da
kalio007 <89003403+kalio007@users.noreply.github.com>
2022-03-03 11:41:14 +0100

    Create helloworld.sol

---
+++ helloworld.sol
@@ -0,0 +1,6 @@
+// SPDX-License-identifier: MIT
+pragma solidity 0.8.7;
+
+contract HelloWorld{
+    string public mystring = "hello World"; // in other to make a variable visible we use the keyword string
+}
